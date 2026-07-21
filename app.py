afe9ad194cea58eb43d3bff1f481dd8b162fe4f9
Sezer Mikail Sagir <sezersagir12@gmail.com>
2026-07-21 14:48:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -178,4 +178,4 @@ if __name__ == "__main__":
     print("  REAL ESTATE TOOL — Web-Interface")
     print("  http://localhost:5000")
     print("=" * 50 + "\n")
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
